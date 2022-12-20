c479d7e80d3076ed4c4a8e02e9479d998ea42849
bebebeko <ryabin.ss@gmail.com>
2022-12-20 23:19:56 +0100

    xterm -e compiled/mindwm/start_freeplane_node_shel.sh ${NODE_ID}

---
+++ templates/mindwm/start_freeplane_node_shell.sh
@@ -0,0 +1,34 @@
+{%- set p = inventory.parameters -%}
+#!/usr/bin/env bash
+
+NODE_ID=$1
+
+test -z "${NODE_ID}" && {
+  cat<<EOF > /dev/stderr
+usage $0 \${NODE_ID}
+for exmaple:
+$0 ID_1090958577
+EOF
+  exit 1
+} 
+
+cd {{ p.kapitan_root }}
+
+NEW_KTARGET_NAME="fp-shell-${NODE_ID}"
+
+
+test -d inventory/targets/freeplane-nodes-sh || mkdir -p inventory/targets/freeplane-node-sh
+
+# inventory/targets/freeplane-node.yml used as a template 
+# use jq for this
+cat inventory/targets/freeplane-node.yml | sed "
+  s,target_name: .*,target_name: ${NEW_KTARGET_NAME},;
+" | tee inventory/targets/freeplane-node-sh/${NEW_KTARGET_NAME}.yml
+
+./kapitan.sh compile --fetch --cache -t ${NEW_KTARGET_NAME}
+
+. compiled/${NEW_KTARGET_NAME}/functions.bash
+tmux_session_start
+# for some reason, tmuxinator attach fails to tmux attach 
+tmux attach -t $NEW_KTARGET_NAME
+
