f96f3497f8574c61ca9c20ad7efa0534756651ac
bebebeko <ryabin.ss@gmail.com>
2022-12-21 02:34:40 +0100

    Workaround for https://github.com/kapicorp/kapitan/pull/912

--- kapitan.sh
+++ kapitan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-KAPITAN_IMAGE="kapicorp/kapitan:v0.30.0"
-#KAPITAN_IMAGE="metacoma/kapitan:latest"
+#KAPITAN_IMAGE="kapicorp/kapitan:v0.30.0"
+KAPITAN_IMAGE="metacoma/kapitan:latest"
 
 kapitan() {
   docker run -t --rm -u $(id -u) -v $(pwd):/src:delegated ${KAPITAN_IMAGE} $*
