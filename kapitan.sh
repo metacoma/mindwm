2622762b402a6cd2c0d09ed6ae8f1c286613ed80
bebebeko <ryabin.ss@gmail.com>
2022-12-24 01:22:58 +0100

    first working fluentd template

--- kapitan.sh
+++ kapitan.sh
@@ -4,7 +4,7 @@
 KAPITAN_IMAGE="metacoma/kapitan:latest"
 
 kapitan() {
-  docker run -t -w `pwd` --rm -u $(id -u) -v $(pwd):`pwd`:delegated ${KAPITAN_IMAGE} $*
+	docker run -t -w `pwd` --rm -u $(id -u):$(id -g) -v/tmp:/tmp -v $(pwd):`pwd`:delegated ${KAPITAN_IMAGE} $*
 }
 
 kapitan $*
