70a9608ed35772f045ca42e6e94d749f48a63d36
Sergey Ryabin <sryabin@juniper.net>
2023-03-05 13:24:56 +0100

    Switchback using upstream kapitan docker image, update kapitan to version 0.31.0

--- kapitan.sh
+++ kapitan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#KAPITAN_IMAGE="kapicorp/kapitan:v0.30.0"
-KAPITAN_IMAGE="metacoma/kapitan:latest"
+KAPITAN_IMAGE="kapicorp/kapitan:v0.31.0"
+#KAPITAN_IMAGE="metacoma/kapitan:latest"
 
 kapitan() {
 	docker run -t -w `pwd` --rm -u $(id -u):$(id -g) -v/tmp:/tmp -v $(pwd):`pwd`:delegated ${KAPITAN_IMAGE} $*
