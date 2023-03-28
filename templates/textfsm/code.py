7a59dc9a31c4ec5ba78f724b5ff5836d11f02d52
Ryabin Sergey <ryabin.ss@gmail.com>
2023-03-28 16:29:48 +0200

    Fix typo in textFSM expression

--- templates/textfsm/code.py
+++ templates/textfsm/code.py
@@ -4,6 +4,9 @@ import json
 import pprint
 import os
 import tabulate
+import textfsm
+import io
+from tabulate import tabulate
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 
@@ -18,15 +21,16 @@ patterns["{{ ctx.input }}"] = re.compile("{{ ctx.input }}")
 for message in consumer:
     msg = json.loads(message.value)
     try:
-        intput_data = msg['input']
+        input_data = msg['input']
         output_data = msg['output']
     except KeyError:
         print("KeyError")
         continue
 {% for ctx in p["io-context"] %}
     if (patterns["{{ ctx.input }}"].match(input_data)):
-        template="""{{ctx.textfsm}}"""
+        textfsm_template="""{{ ctx.textfsm }}"""
         re_table = textfsm.TextFSM(io.StringIO(textfsm_template))
+        header = re_table.header
         result = re_table.ParseText(output_data)
         print(tabulate(result, headers=header))
 {% endfor %}
