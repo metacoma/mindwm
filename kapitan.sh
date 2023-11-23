438db002b4e02f50a88e78536a3f1c17695dd8af
Ryabin Sergey <ryabin.ss@gmail.com>
2023-11-23 23:53:54 +0100

    test localhook

--- kapitan.sh
+++ kapitan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 KAPITAN_IMAGE="kapicorp/kapitan:v0.32.0"
-#KAPITAN_IMAGE="metacoma/kapitan:latest"
+##KAPITAN_IMAGE="metacoma/kapitan:latest"
 
 kapitan() {
 	docker run -t -w `pwd` --rm -u $(id -u):$(id -g) -v/tmp:/tmp -v $(pwd):`pwd`:delegated ${KAPITAN_IMAGE} $*
